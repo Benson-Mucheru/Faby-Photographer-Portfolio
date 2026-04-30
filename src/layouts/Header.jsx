4da8a12b0e41342a0730122cd19995c5074f51e4
Benson Mucheru <benmk300@gmail.com>
2026-04-30 12:49:22 +0300

    feat: added a sidebar on the mobile screen

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -6,7 +6,100 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
   return (
-    <nav className="sticky top-0 z-3 w-full bg-black/90 text-white backdrop-blur-md">
+    <>
+      <div className="drawer sticky top-0 z-3 w-full text-white backdrop-blur-md">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div className="navbar w-full bg-black/90">
+            <div className="mx-2 flex-1 px-2">
+              <Link to="/">
+                <h2
+                  className="font-bold font-sora text-2xl pt-3"
+                  style={{ lineHeight: 0.5 }}
+                >
+                  Mr. Sololobo
+                </h2>
+                <h3 className="text-lg font-bold">PicZel Pictures</h3>
+              </Link>
+            </div>
+
+            <div className="hidden flex-none lg:block">
+              <ul className="menu menu-horizontal">
+                {/* Navbar menu content here */}
+                <li>
+                  <button
+                    popoverTarget="gallaries"
+                    style={{ anchorName: "category" }}
+                  >
+                    Gallary
+                  </button>
+
+                  <ul
+                    className="dropdown menu w-52 rounded-box bg-base-100 shadow-sm"
+                    popover="auto"
+                    id="gallaries"
+                    style={{ positionAnchor: "category" }}
+                  >
+                    <li className="text-black">
+                      <Link to="/category/wedding">wedding</Link>
+                    </li>
+                    <li className="text-black">
+                      <Link to="/category/potrait">potrait</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>About</a>
+                </li>
+
+                <li>
+                  <a>Contact</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-2"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-2"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-black min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* <nav className="sticky top-0 z-3 w-full bg-black/90 text-white backdrop-blur-md">
       <header className="flex justify-between items-center py-2 relative mx-auto max-w-350 not-md:px-1.5">
         <Link to="/">
           <h2
@@ -58,6 +151,7 @@ export default function Header() {
           menu
         </button>
       </header>
-    </nav>
+    </nav> */}
+    </>
   );
 }
